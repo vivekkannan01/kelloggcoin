ac4a33e400e48392b3a84d76dc0102bfb659638c
Vivek Kannan <vivek.kannan@kellogg.northwestern.edu>
2025-04-27 22:50:19 +0000

    completed pratice problem

--- practice.rb
+++ practice.rb
@@ -27,3 +27,47 @@ blockchain = [
 # Anthony's KelloggCoin balance is 2650
 
 # 👇👇👇 Your code HERE 👇👇👇
+
+ben = 0
+brian = 0
+evan = 0
+anthony = 0
+
+index = 0
+
+loop do 
+  
+  if index == blockchain.count
+    break
+  end
+
+  if blockchain [index]["from_user"] == "ben"
+    ben = ben - blockchain[index]["amount"] 
+  elsif blockchain [index]["from_user"] == "brian" 
+    brian = brian - blockchain[index]["amount"] 
+  elsif blockchain [index]["from_user"] == "evan"
+    evan = evan - blockchain[index]["amount"] 
+  elsif blockchain [index]["from_user"] == "anthony"
+    anthony = anthony - blockchain[index]["amount"]
+  end
+  
+  if blockchain [index]["to_user"] == "ben"
+    ben = ben + blockchain[index]["amount"] 
+  elsif blockchain [index]["to_user"] == "brian"
+    brian = brian + blockchain[index]["amount"] 
+  elsif blockchain [index]["to_user"] == "evan"
+    evan = evan + blockchain[index]["amount"] 
+  elsif blockchain [index]["to_user"] == "anthony"
+    anthony = anthony + blockchain[index]["amount"]
+  end
+
+  index = index + 1
+
+end
+
+puts ""
+puts "Ben's KelloggCoin balance is #{ben}"
+puts "Brian's KelloggCoin balance is #{brian}"
+puts "Evan's KelloggCoin balance is #{evan}"
+puts "Anthony's KelloggCoin balance is #{anthony}"
+puts ""
